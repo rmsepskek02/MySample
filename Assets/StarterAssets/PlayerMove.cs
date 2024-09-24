f7db69569fd96aa31aed91559aab48e5fe6e8765
rmsepskek02 <rmsepskek02@gmail.com>
2024-09-24 14:16:08 +0900

    init
    init

---
+++ Assets/StarterAssets/PlayerMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+/*
+캐릭터 컨트롤
+ - 빌트인 Character Controller
+ - Rigidbody : 다이나믹
+ - Rigidbody : 키네메틱
+
+  
+ */
